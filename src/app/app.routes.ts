b2fc1b2102e2090f6fd9a4a9a883e2c5d4cd3392
Cipriano Gorosito <nahuelgorosito1983@gmail.com>
2025-09-28 04:05:43 +0000

    feat: estructura inicial de páginas, layout y dark mode con Tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
+    { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact) },
+    { path: 'cv', loadComponent: () => import('./pages/cv/cv').then(m => m.Cv) },
+    { path: 'blog', loadComponent: () => import('./pages/blog/blog').then(m => m.Blog) },
+    { path: 'projects', loadComponent: () => import('./pages/projects/projects').then(m => m.Projects) },
+    { path: 'media', loadComponent: () => import('./pages/media/media').then(m => m.Media) },
+];
